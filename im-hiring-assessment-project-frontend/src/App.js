10861e3947dcbd0a29b1bc0cc182ff64b28fcdef
Sanghoon Han <bnnj86@gmail.com>
2019-12-11 16:37:18 +0900

    add route, css, etc

--- im-hiring-assessment-project-frontend/src/App.js
+++ im-hiring-assessment-project-frontend/src/App.js
@@ -1,10 +1,19 @@
 import React, { Component } from 'react';
+import { Route, Switch } from 'react-router-dom';
+import { Home, Board, Todo, User } from './pages';
+import './App.css';
 
 class App extends Component {
   render() {
     return (
-      <div>
-        App
+      <div className = "App">
+        <Route exact path="/" component={Home} />
+        <Switch>
+          <Route exact path="/Todo" component={Todo} />
+          <Route path="/Board/:list" component={Board} />
+          <Route path="/Board" component={Board} />
+          <Route path="/User" component={User} />
+        </Switch>
       </div>
     );
   }
