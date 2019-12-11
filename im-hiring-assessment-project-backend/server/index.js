9a035b8d481a416388ae6915dab6092711ad9759
Sanghoon Han <bnnj86@gmail.com>
2019-12-11 14:44:55 +0900

    server add

--- im-hiring-assessment-project-backend/server/index.js
+++ im-hiring-assessment-project-backend/server/index.js
@@ -1,8 +1,46 @@
+const http = require('http');
 const express = require('express');
+const cors = require('cors');
+const morgan = require('morgan');
+const parser = require('body-parser');
+const cookieParser = require('cookie-parser');
+const router = require('./routes.js');
 const app = express();
+const port = 4000;
+const session = require('express-session');
+//db access => use sequelize
+// const db = require('./db');
 
-app.get('/test', async (req, res) => {
-  res.json({ message: 'pass!' });
+app.set('port', port); // Set app, 5000 port;
+console.log('server is listen on', port);
+
+app.use(morgan('dev'));
+app.use(cookieParser());
+app.use(parser.json());
+app.use(parser.urlencoded({ extended: false }));
+// app.use(cors({
+//   origin: "",
+//   credentials: true
+// }));
+
+app.use(
+  session({
+    key: 'b6',
+    secret: '@haserver',
+    resave: false,
+    saveUninitialized: true,
+    cookie: {
+      maxAge: 1000 * 60 * 60,
+    },
+  }),
+);
+
+app.use('/', router);
+
+app.get('/', (req, res) => {
+  res.status(200).send('HelloWorld!, HA test server!');
 });
+
+app.listen(app.get('port'));
 
 module.exports = app;
